7b6c120d28a569922519ba98cf318b13de666ae2
nayakdebananda <rdebanandanayak@gmail.com>
2025-03-07 14:46:33 +0530

    add exit 0

--- app/main.go
+++ app/main.go
@@ -20,6 +20,9 @@ func main() {
 			fmt.Fprintln(os.Stderr, "Error reading input", err)
 			os.Exit(1)
 		}
+		if command == "exit 0\n" {
+			os.Exit(1)
+		}
 		fmt.Println(command[:len(command)-1] + ": command not found")
 	}
 }
